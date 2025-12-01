8434c4578933dcbe55eae95b7e705bd9d10ed3bf
igoreduardobraga <igoredubraga@gmail.com>
2025-11-30 21:47:24 -0300

    feat: add first map transition

--- Source/Game.h
+++ Source/Game.h
@@ -53,7 +53,6 @@ public:
     void PushUI(UIScreen* screen);
     std::vector<UIScreen*>& GetUIStack() { return mUIStack; }
 
-    // Audio
     AudioSystem* GetAudio() { return mAudio; }
 
     void SetScene(GameScene scene);
@@ -106,6 +105,7 @@ private:
     void GenerateOutput();
     void UpdateActors(float deltaTime);
     void UpdateCamera();
+    void CheckExitReached();
     class HUD* mHUD;
 
     void InitializeActors();
@@ -131,9 +131,11 @@ private:
     std::vector<UIScreen*> mUIStack;
 
     Vector2 mCameraPos;
+    std::vector<Vector2> mExitTiles;
+    std::string mMapPrefix;
 
     FadeState mFadeState;
-    float mFadeAlpha;     // 0.0f (transparente) a 1.0f (preto)
+    float mFadeAlpha;
     GameScene mNextScene;
 
     // Audio system
